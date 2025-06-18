06a57f84d9a70e7d944e260a32ded865ceac401c
snosnosno <tmdgh1qn@naver.com>
2025-06-19 00:36:21 +0900

    feat: 관리자 인증 및 기본 Admin 로그인 UI, Firebase 연동, Tailwind 세팅

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminLogin from './components/AdminLogin';
+
+function App() {
+  return <AdminLogin />;
+}
+
+export default App;
+
+  );
+}
+
+export default App;
